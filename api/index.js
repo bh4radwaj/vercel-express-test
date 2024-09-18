529a1fe852494a90847cba08a88362d98589a0cb
Your Name <your.email@example.com>
2024-09-18 12:17:24 +0530

    changed the structure to /api

--- api/index.js
+++ api/index.js
@@ -4,15 +4,10 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
-
-// Send index.html for the root URL
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.send('Hello from Express on Vercel!');
 });
 
-// Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
