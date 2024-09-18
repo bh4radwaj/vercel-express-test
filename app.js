f564f349fc0f21fc6e2e37f0ad17cf8f1cfa557d
Your Name <your.email@example.com>
2024-09-18 12:47:55 +0530

    added simple template

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
+// index.js
 const express = require('express');
+const path = require('path');
 
-const app = express()
-const PORT = 8000
+const app = express();
+const PORT = process.env.PORT || 3000;
 
+// Serve the HTML file
 app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-app.get('/about', (req, res) => {
-  res.send('About route 🎉 ')
-})
-
+// Start the server
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
-})
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
